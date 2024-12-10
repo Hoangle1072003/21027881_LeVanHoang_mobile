963873b0c04b35543fd303d74a3b8e77ffce56de
ashleynguci <hoangkinboy@gmail.com>
2024-12-11 01:46:49 +0700

    add home

--- frontend/screens/home.js
+++ frontend/screens/home.js
@@ -1,10 +1,11 @@
-import { Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { getAllUser } from "../services/userService";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUsers } from "../redux/userSlice";
 const Home = () => {
   const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.user);
   const fetchAllUser = async () => {
     try {
       const res = await getAllUser();
@@ -17,9 +18,40 @@ const Home = () => {
   useEffect(() => {
     fetchAllUser();
   }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <>
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            flex: 1,
+            backgroundColor: "red",
+            margin: 10,
+          }}
+        >
+          <Text>{item.name}</Text>
+        </View>
+      </>
+    );
+  };
   return (
     <>
-      <Text>Home</Text>
+      <View
+        style={{
+          marginHorizontal: 20,
+          marginVertical: 20,
+        }}
+      >
+        <FlatList
+          data={users}
+          renderItem={renderItem}
+          // showsHorizontalScrollIndicator
+          // horizontal
+          numColumns={3}
+        />
+      </View>
     </>
   );
 };
